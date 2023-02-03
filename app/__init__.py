1deae55d030205fc5a411f246a2998f77231abfb
Oscar Santos <oscarsantosmu@gmail.com>
2023-02-03 19:27:46 +0100

    feat(components): Add experience component
    Add component whose information is filled in with sample data
    
    Refs: #3

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from .models.experience import Experience
+from .static.sample_data.data import data
 
 load_dotenv()
 app = Flask(__name__)
 
 
+experiences = []
+for experience in data['experience']:
+    experiences.append(Experience(experience['company'], 
+                                  experience['position'], 
+                                  experience['duration'], 
+                                  experience['description']
+                                  )
+                        )
+
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('components/experience.html', title="MLH Fellow", url=os.getenv("URL"), experiences=experiences)
