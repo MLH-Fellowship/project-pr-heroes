95de0846c958172a053a0725868bc5c3224ce1ad
Oscar Santos <oscarsantosmu@gmail.com>
2023-02-22 18:52:40 -0600

    feat(database): Add ORM and Db model.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,25 @@
-from flask import Flask, request
+import os
+
 from dotenv import load_dotenv
+from flask import Flask, request
+from peewee import *
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306,
+)
+
+print(mydb)
+
+from .models.timelinepost import TimelinePost
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 from app import routes
