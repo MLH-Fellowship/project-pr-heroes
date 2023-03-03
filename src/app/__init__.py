d25bf323609ebcc8e37d44b393d88fc944109ae2
Khalil Collins <Collinskhalil@hotmail.com>
2023-03-03 13:41:38 -0500

    added logic to choose which database to use

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,8 @@
-from app import routes
+
 from flask import Flask, request
 from dotenv import load_dotenv
+import datetime
 
-from .models.timelinepost import TimelinePost
 import os
 
 
@@ -12,15 +12,33 @@ load_dotenv()
 app = Flask(__name__)
 # app.config["SECRET_KEY"] = os.getenv("APP_SECRET")
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=4000,
-)
+if os.getenv('TESTING') == "true":
+    print('Running in test mode')
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared',
+                          uri=True)
+else:
+    mydb = MySQLDatabase(
+        os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=4000,
+    )
 
+
+
+
+class TimelinePost(Model):
+
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
 
 mydb.connect()
 mydb.create_tables([TimelinePost])
+from app import routes
 print(mydb)
