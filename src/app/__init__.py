aa3fe1a2952a09204f80db38824a20bd09c219db
Khalil Collins <Collinskhalil@hotmail.com>
2023-03-03 10:39:51 -0500

    changed port in init file for db

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,26 +1,27 @@
+
+from app import routes
+from flask import Flask, request
+from dotenv import load_dotenv
+
+from .models.timelinepost import TimelinePost
 import os
 
-from dotenv import load_dotenv
-from flask import Flask, request
+
 from peewee import *
 
 load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("APP_SECRET")
+# app.config["SECRET_KEY"] = os.getenv("APP_SECRET")
 
 mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
     host=os.getenv("MYSQL_HOST"),
-    port=3306,
+    port=4000,
 )
 
-print(mydb)
-
-from .models.timelinepost import TimelinePost
 
 mydb.connect()
 mydb.create_tables([TimelinePost])
-
-from app import routes
+print(mydb)
