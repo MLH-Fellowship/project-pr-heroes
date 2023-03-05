49368ec81b2c300425d715e62c01e643f1be33f0
Khalil Collins <Collinskhalil@hotmail.com>
2023-03-05 08:45:34 -0500

    changed port back to 3306

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -22,7 +22,7 @@ else:
         user=os.getenv("MYSQL_USER"),
         password=os.getenv("MYSQL_PASSWORD"),
         host=os.getenv("MYSQL_HOST"),
-        port=4000,
+        port=3306,
     )
 
 
